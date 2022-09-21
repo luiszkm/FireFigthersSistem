72231d8c59ee6db53d2294364d21107707d56c94
luis <luiszkm@gmail.com>
2022-09-21 13:23:00 -0300

    att footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,13 @@
 import { Container } from "./styles";
-import{FiHexagon} from "react-icons/fi"
+import{GiWolfHead} from "react-icons/gi"
 export function Footer (){
 
   return(
     <Container>
       <small>
        
-        <h4> <FiHexagon size={20} fill="#7C7C8A" /> 
-        Food Explorer</h4>
+        <h4> <GiWolfHead size={20} fill="#7C7C8A" /> 
+        SC</h4>
       &copy;-2022- Todos os Direitos reservados
         <a target="_blank" href="https://devsoares.com">Desenvolvido por Luis Soares</a>
       </small>
