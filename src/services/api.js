8856f9bcbe9aa39a58fc94e81a369ba6f69fc9a1
luis <luiszkm@gmail.com>
2022-09-23 11:10:55 -0300

    att api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://mall-fireman-ebh-api.herokuapp.com/'
+  baseURL: 'http://localhost:3333'
+  //baseURL: 'https://mall-fireman-ebh-api.herokuapp.com/'
 })
 
